b7280f29f67cf9fab3393a0ea375aabd99412447
rolissovi <romalis717@gmail.com>
2024-04-24 23:04:25 +0300

    First Commit

--- scripts/data_collect.py
+++ scripts/data_collect.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pymongo import MongoClient
 
 print("Hi. Commit it")
+print("Hi. Commit it")
 def amount_statistics(params):
     print("Среднее занчение: ", statistics.mean(params))
     print("Минимальное значение: ", min(params))
